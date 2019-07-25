92515cf869f82f49499883f5930e9857ba2b9e96
Eros Fratini <eros.fratini@convergence1.com>
2019-07-25 16:35:28 +0200

    Initial commit

---
+++ src/dotnet-figlet.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using Figgle;
+using System;
+
+namespace dotnet_figlet.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FiggleFonts.Alligator.Render("Hello World!"));
+        }
+    }
+}
