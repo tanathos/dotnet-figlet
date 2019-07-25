afd03ca97d749d009f9cf657d1fb21fb749ea3d7
Eros Fratini <eros.fratini@gmail.com>
2019-07-26 00:37:47 +0200

    Colors and various fixes

--- src/dotnet-figlet.Console/Program.cs
+++ src/dotnet-figlet.Console/Program.cs
@@ -13,43 +13,54 @@ namespace dotnet_figlet.console
     {
         static void Main(string[] args)
         {
+            // Workaround for Colorful.Console issue https://github.com/tomakita/Colorful.Console/issues/16
+            _resetDefaultColors();
+
             bool showHelp = false;
             string font = "standard";
+            string color = "color";
 
             var options = new OptionSet()
             {
                 { "h|help|?", v => { showHelp = true; } },
-                { "f|font=", v => { font = v; } }
+                { "f|font=", v => { font = v; } },
+                { "c|color=", v => { color = v; } }
             };
 
             var parsed = options.Parse(args);
 
-            var assembly = typeof(Program).GetTypeInfo().Assembly;
+            Color renderColor = Color.FromName(color);
+            if (!renderColor.IsKnownColor)
+                renderColor = Console.ForegroundColor;
 
             Console.WriteLine($"[Loading font]: dotnet_figlet.Console.Fonts.{font}.flf");
+            Console.WriteLine($"[Color]: {renderColor.Name}");
 
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
             Stream resource = assembly.GetManifestResourceStream($"dotnet_figlet.Console.Fonts.{font}.flf");
 
             FigletFont figletFont = FigletFont.Load(resource);
             Figlet figlet = new Figlet(figletFont);
 
-            List<string> extra;
+            Console.WriteLine($"[Figlet font]: MaxLength={figletFont.MaxLength}, Height={figletFont.Height}, FullLayout={figletFont.FullLayout}, BaseLine={figletFont.BaseLine}");
+
+            List<string> extraparams;
             try
             {
-                extra = options.Parse(args);
+                extraparams = options.Parse(args);
             }
             catch (OptionException e)
             {
-                Console.Write("subclass: ");
+                Console.Write("Invalid option(s): ");
                 Console.WriteLine(e.Message);
                 return;
             }
 
             string message = "";
 
-            if (extra.Count > 0)
+            if (extraparams.Count > 0)
             {
-                message = string.Join(" ", extra.ToArray());
+                message = string.Join(" ", extraparams.ToArray());
                 Console.WriteLine($"[Input]: {message}");
             }
 
@@ -57,15 +68,40 @@ namespace dotnet_figlet.console
             {
                 _showHelp();
             }
+            
+            // TODO: calculate console width and rearrange the string tokens to newlines
 
-            Console.WriteLine(figlet.ToAscii(message).ToString(), Color.Blue);
+            Console.WriteLine(figlet.ToAscii(message).ToString(), renderColor);
 
-            // Console.WriteLine(FiggleFonts.Alligator.Render(message));
+            Environment.Exit(0);
         }
 
         private static void _showHelp()
         {
-            Console.WriteLine("Usage: figlet <sentence to figlettize>");
+            Console.WriteLine("Usage: figlet [options] <sentence to figlettize>");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("\t-h, --help\t\t\tShow this page");
+            Console.WriteLine("\t-f, --font <font name>\t\tFiglet font to use for render, 'standard' is the default");
+            Console.WriteLine("\t-c, --color <color name>\tColor to use");
+        }
+
+        private static void _resetDefaultColors()
+        {
+            Console.Write("", Color.Navy);
+            Console.Write("", Color.Green);
+            Console.Write("", Color.Teal);
+            Console.Write("", Color.Maroon);
+            Console.Write("", Color.Purple);
+            Console.Write("", Color.Olive);
+            Console.Write("", Color.Silver);
+            Console.Write("", Color.Gray);
+            Console.Write("", Color.Blue);
+            Console.Write("", Color.Lime);
+            Console.Write("", Color.Cyan);
+            Console.Write("", Color.Red);
+            Console.Write("", Color.Fuchsia);
+            Console.Write("", Color.Yellow);
         }
     }
 }
