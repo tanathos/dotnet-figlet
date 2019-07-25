9f0fddcd1563117d4a61206f3a2eaecad33c1983
Eros Fratini <eros.fratini@gmail.com>
2019-07-25 20:42:50 +0200

    Embedded figlet fonts, some basic stuff

--- src/dotnet-figlet.Console/Program.cs
+++ src/dotnet-figlet.Console/Program.cs
@@ -1,5 +1,11 @@
-﻿using Figgle;
+﻿using Colorful;
+using NDesk.Options;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using Console = Colorful.Console;
 
 namespace dotnet_figlet.console
 {
@@ -7,7 +13,59 @@ namespace dotnet_figlet.console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FiggleFonts.Alligator.Render("Hello World!"));
+            bool showHelp = false;
+            string font = "standard";
+
+            var options = new OptionSet()
+            {
+                { "h|help|?", v => { showHelp = true; } },
+                { "f|font=", v => { font = v; } }
+            };
+
+            var parsed = options.Parse(args);
+
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+
+            Console.WriteLine($"[Loading font]: dotnet_figlet.Console.Fonts.{font}.flf");
+
+            Stream resource = assembly.GetManifestResourceStream($"dotnet_figlet.Console.Fonts.{font}.flf");
+
+            FigletFont figletFont = FigletFont.Load(resource);
+            Figlet figlet = new Figlet(figletFont);
+
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("subclass: ");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            string message = "";
+
+            if (extra.Count > 0)
+            {
+                message = string.Join(" ", extra.ToArray());
+                Console.WriteLine($"[Input]: {message}");
+            }
+
+            if (showHelp)
+            {
+                _showHelp();
+            }
+
+            Console.WriteLine(figlet.ToAscii(message).ToString(), Color.Blue);
+
+            // Console.WriteLine(FiggleFonts.Alligator.Render(message));
+        }
+
+        private static void _showHelp()
+        {
+            Console.WriteLine("Usage: figlet <sentence to figlettize>");
         }
     }
 }
